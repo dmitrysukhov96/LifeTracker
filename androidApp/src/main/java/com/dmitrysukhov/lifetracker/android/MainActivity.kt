d9ff4b88f68d4895723b659faac4d5e4fde42f77
dima <dmitrysukhov96@gmail.com>
2024-11-15 23:34:16 +0200

    trying to commit from another account

--- androidApp/src/main/java/com/dmitrysukhov/lifetracker/android/MainActivity.kt
+++ androidApp/src/main/java/com/dmitrysukhov/lifetracker/android/MainActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.dmitrysukhov.lifetracker.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +32,4 @@ fun GreetingView(text: String) {
     Text(text = text)
 }
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
-}
+//hello i'm trying to test coworking in GitHub :)
