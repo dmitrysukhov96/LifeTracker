988258c5cdafd7c4b1723f9f1ccf9e1754af6fdf
Skover3563 <kostiaostrouhov6e@gmail.com>
2024-11-21 17:28:10 +0200

    Сделал Нижнюю панель

---
+++ androidApp/src/main/java/com/dmitrysukhov/lifetracker/android/DataClass.kt
@@ -0,0 +1,13 @@
+package com.dmitrysukhov.lifetracker.android
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+
+
+)
--- androidApp/src/main/java/com/dmitrysukhov/lifetracker/android/MainActivity.kt
+++ androidApp/src/main/java/com/dmitrysukhov/lifetracker/android/MainActivity.kt
@@ -4,35 +4,119 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import com.dmitrysukhov.lifetracker.Greeting
+import org.w3c.dom.Text
+
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "TimeTracker",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false,
+
+                        ),
+                    BottomNavigationItem(
+                        title = "TODO",
+                        selectedIcon = Icons.Filled.DateRange,
+                        unselectedIcon = Icons.Outlined.DateRange,
+                        hasNews = false,
+                        badgeCount = 45
+
+                    ),
+
+                    BottomNavigationItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Filled.Settings,
+                        unselectedIcon = Icons.Outlined.Settings,
+                        hasNews = true,
+
+                        ),
+                )
+
+
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     MainScreen(Greeting().greet())
+                }
+                val selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+
+                }
+                Scaffold(bottomBar = {
+                    NavigationBar {
+                        items.forEachIndexed { index, item ->
+                            NavigationBarItem(selected = selectedItemIndex == index, onClick = {
+                                selectedItemIndex == index
+                                // navControler.navigate(item.title)
+                            },
+                                label = {
+                                    Text(text = item.title)
+                                },
+                                alwaysShowLabel = false,
+                                icon = {
+                                BadgedBox(badge = {
+                                    if (item.badgeCount != null) {
+                                        Badge {
+                                            Text(text = item.badgeCount.toString())
+                                        }
+                                    } else if (item.hasNews) {
+                                        Badge()
+                                    }
+                                }) {
+                                    Icon(
+                                        imageVector = if (index == selectedItemIndex) {
+                                            item.selectedIcon
+                                        } else item.unselectedIcon,
+                                        contentDescription = item.title
+                                    )
+                                }
+                            })
+                        }
+                    }
+                }) {
+
                 }
             }
         }
     }
 }
 
+
 @Composable
 fun MainScreen(text: String) {
-
 }
+
 
 //Timofey
 //сава тест 2
