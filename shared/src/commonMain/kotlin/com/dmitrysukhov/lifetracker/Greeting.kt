e0dfbe7614692692ee8e61cfec223e13685f6e62
dima <dmitrysukhov96@gmail.com>
2024-11-28 13:57:25 +0200

    init commit

--- shared/src/commonMain/kotlin/com/dmitrysukhov/lifetracker/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.dmitrysukhov.lifetracker
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
