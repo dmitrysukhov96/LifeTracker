cff56d20bed7f6f816d919b9af9daf83004016cf
Skover3563 <kostiaostrouhov6e@gmail.com>
2024-12-27 13:41:47 +0200

    Сделал Кнопку гоу

--- app/src/main/java/com/dmitrysukhov/lifetracker/MainActivity.kt
+++ app/src/main/java/com/dmitrysukhov/lifetracker/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,6 +29,8 @@ import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -41,7 +45,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
@@ -126,9 +133,37 @@ class MainActivity : ComponentActivity() {
                             }
                         },
                         floatingActionButton = {
-                            //todo тут добавить кнопку ГОУ!!
+                            Row(
+                                modifier = Modifier
+                                    .height(56.dp)
+                                    .width(86.dp)
+                                    .background(
+                                        color = Color(0xFF33BA78),
+                                        shape = RoundedCornerShape(50.dp)
+                                    )
+
+                                    .clip(RoundedCornerShape(50.dp))
+                                    .clickable {  }
+                                    .padding(horizontal = 12.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(id = R.drawable.molni),
+                                    contentDescription = null,
+                                    tint = Color.Yellow,
+                                    modifier = Modifier.size(20.dp)
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(
+                                    text = "GO!",
+                                    fontSize = 18.sp,
+                                    color = Color.White,
+                                            fontWeight = FontWeight.W700
+                                )
+                            }
                         }
-                        ) { padding ->
+
+                    ) { padding ->
                         Box(Modifier.fillMaxSize()) {
 
                             NavHost(
