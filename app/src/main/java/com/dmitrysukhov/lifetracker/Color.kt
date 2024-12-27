1e90d34e3c75b355e6d1bfb6ce06a27c2d0bd943
dima <dmitrysukhov96@gmail.com>
2024-12-27 15:42:54 +0200

    some changes

--- app/src/main/java/com/dmitrysukhov/lifetracker/Color.kt
+++ app/src/main/java/com/dmitrysukhov/lifetracker/Color.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-val PineColor = Color(0xFFC2EBD6)
-val DarkerPine = Color(0xFF18593A)
-val AccentColor = Color(0xFFCBF66E)
 val WhitePine = Color(0xFFF1FCF5)
+val PineColor = Color(0xFFC2EBD6)
+val AccentColor = Color(0xFFCBF66E)
+val DarkerPine = Color(0xFF18593A)
 val BlackPine = Color(0xFF0E281C)
 val Yellow = Color(0xFFFFEA00)
 val BgColor @Composable get() = if (isDarkTheme()) BlackPine else WhitePine
