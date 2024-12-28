0d4df1d425b5d37a4c4243ebf8fbe2d292bcc2ae
dima <dmitrysukhov96@gmail.com>
2024-12-28 16:22:48 +0200

    вернул гоу

--- app/src/main/java/com/dmitrysukhov/lifetracker/MainActivity.kt
+++ app/src/main/java/com/dmitrysukhov/lifetracker/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -17,6 +23,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
@@ -35,6 +42,7 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -42,11 +50,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle.Companion.Italic
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight.Companion.ExtraBold
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
@@ -102,8 +112,8 @@ class MainActivity : ComponentActivity() {
                                     .height(56.dp)
                             ) {
                                 Text(
-                                    "LifeTracker", style = InvolveBold,
-                                    fontSize = 20.sp,
+                                    "LifeTracker", fontFamily = Montserrat,
+                                    fontSize = 20.sp, fontWeight = Bold,
                                     color = WhitePine,
                                     modifier = Modifier.align(Alignment.Center)
                                 )
@@ -130,37 +140,7 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
                         },
-                        floatingActionButton = {
-                            Row(
-                                modifier = Modifier
-                                    .height(56.dp)
-                                    .width(86.dp)
-                                    .background(
-                                        color = Color(0xFF33BA78),
-                                        shape = RoundedCornerShape(50.dp)
-                                    )
-
-                                    .clip(RoundedCornerShape(50.dp))
-                                    .clickable { }
-                                    .padding(horizontal = 12.dp),
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Icon(
-                                    imageVector = ImageVector.vectorResource(id = R.drawable.molni),
-                                    contentDescription = null,
-                                    tint = Color.Yellow,
-                                    modifier = Modifier.size(20.dp)
-                                )
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Text(
-                                    text = "GO!",
-                                    fontSize = 18.sp,
-                                    color = Color.White,
-                                    fontWeight = FontWeight.W700
-                                )
-                            }
-                        }
-
+                        floatingActionButton = { ActuallyFloatingActionButton({ /*todo click*/ }) }
                     ) { padding ->
                         Box(Modifier.fillMaxSize()) {
 
@@ -191,17 +171,17 @@ class MainActivity : ComponentActivity() {
                             ) {
                                 Column {
                                     Text(
-                                        "Task123", color = BlackPine,
-                                        style = InvolveBold, fontSize = 18.sp
+                                        "Task123", color = BlackPine, fontWeight = Bold,
+                                        fontFamily = Montserrat, fontSize = 18.sp
                                     )
                                     Text(
-                                        "Project456", color = BlackPine,
-                                        style = InvolveBold, fontSize = 14.sp
+                                        "Project456", color = BlackPine, fontWeight = Bold,
+                                        fontFamily = Montserrat, fontSize = 14.sp
                                     )
                                 }
                                 Text(
-                                    "00:23:57", color = BlackPine,
-                                    style = InvolveBold, fontSize = 20.sp
+                                    "00:23:57", color = BlackPine, fontWeight = Bold,
+                                    fontFamily = Montserrat, fontSize = 20.sp
                                 )
                                 Row {
                                     Box(modifier = Modifier.size(50.dp)) {
@@ -227,5 +207,45 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ActuallyFloatingActionButton(onClick: () -> Unit) {
+    val transition = rememberInfiniteTransition(label = "")
+    val offsetY by transition.animateFloat(
+        initialValue = 0f, targetValue = 5f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1300, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ), label = ""
+    )
+
+    Row(
+        modifier = Modifier
+            .offset(y = offsetY.dp)
+            .height(56.dp)
+            .width(86.dp)
+            .clip(RoundedCornerShape(50.dp))
+            .shadow(2.dp)
+            .background(
+                color = Color(0xFF33BA78),
+                shape = RoundedCornerShape(50.dp)
+            )
+            .clickable { onClick() }
+            .padding(horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(R.drawable.lightning),
+            contentDescription = null, modifier = Modifier.size(16.dp, 22.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = "GO!",
+            fontSize = 18.sp, fontStyle = Italic,
+            color = Color.White, fontFamily = Montserrat,
+            fontWeight = ExtraBold
+        )
     }
 }
