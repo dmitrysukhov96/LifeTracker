317536ae440991b62cc9dbd53cc5c62fcd430aa7
dima <dmitrysukhov96@gmail.com>
2025-04-07 23:04:58 +0300

    закомментил часть на пока что

--- app/src/main/java/com/dmitrysukhov/lifetracker/todo/NewTaskScreen.kt
+++ app/src/main/java/com/dmitrysukhov/lifetracker/todo/NewTaskScreen.kt
@@ -3,18 +3,32 @@ package com.dmitrysukhov.lifetracker.todo
 import android.app.TimePickerDialog
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -28,8 +42,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.dmitrysukhov.lifetracker.R
-import com.dmitrysukhov.lifetracker.utils.*
-import java.util.*
+import com.dmitrysukhov.lifetracker.utils.BgColor
+import com.dmitrysukhov.lifetracker.utils.H1
+import com.dmitrysukhov.lifetracker.utils.H2
+import com.dmitrysukhov.lifetracker.utils.InverseColor
+import com.dmitrysukhov.lifetracker.utils.Montserrat
+import com.dmitrysukhov.lifetracker.utils.PineColor
+import com.dmitrysukhov.lifetracker.utils.SimpleText
+import com.dmitrysukhov.lifetracker.utils.TopBarState
+import java.util.Calendar
 
 @Composable
 fun NewTaskScreen(
@@ -39,9 +60,9 @@ fun NewTaskScreen(
     var title by rememberSaveable { mutableStateOf("") }
     var description by rememberSaveable { mutableStateOf("") }
     var selectedTime by rememberSaveable { mutableStateOf("Завтра 15:00") }
-    var reminders by rememberSaveable { mutableStateOf(listOf("Сегодня 22:30", "Завтра 11:00")) }
-    var repeatDays by rememberSaveable { mutableStateOf(setOf<String>()) }
-    var taskDuration by rememberSaveable { mutableStateOf("00:45:00") }
+//    var reminders by rememberSaveable { mutableStateOf(listOf("Сегодня 22:30", "Завтра 11:00")) }
+//    var repeatDays by rememberSaveable { mutableStateOf(setOf<String>()) }
+//    var taskDuration by rememberSaveable { mutableStateOf("00:45:00") }
     val context = LocalContext.current
 
     val globalTextStyle = TextStyle(
@@ -97,7 +118,6 @@ fun NewTaskScreen(
             )
             Spacer(modifier = Modifier.height(8.dp))
             HorizontalDivider()
-
             BasicTextField(
                 value = description, onValueChange = { description = it },
                 textStyle = SimpleText.copy(color = InverseColor),
@@ -115,10 +135,8 @@ fun NewTaskScreen(
                 },
                 modifier = Modifier.fillMaxWidth()
             )
-
             Spacer(modifier = Modifier.height(16.dp))
             HorizontalDivider()
-
             TaskOption(selectedTime, R.drawable.data) {
                 val calendar = Calendar.getInstance()
                 TimePickerDialog(
@@ -132,86 +150,86 @@ fun NewTaskScreen(
                 ).show()
             }
             HorizontalDivider()
-            TaskOption(stringResource(R.string.shopping), R.drawable.proekt, showIcon = true) {}
-            HorizontalDivider()
-            TaskOption(stringResource(R.string.reminders), R.drawable.bell) {}
-
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            ) {
-                reminders.forEach { reminder ->
-                    Row(
-                        modifier = Modifier
-                            .background(AccentColor, shape = RoundedCornerShape(20.dp))
-                            .padding(horizontal = 12.dp, vertical = 6.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(text = reminder, style = SimpleText, color = DarkerPine)
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Icon(
-                            painter = painterResource(id = R.drawable.krest),
-                            contentDescription = null, tint = DarkerPine,
-                            modifier = Modifier
-                                .size(16.dp)
-                                .clickable { reminders = reminders - reminder }
-                        )
-                    }
-                    Spacer(modifier = Modifier.width(8.dp))
-                }
-            }
-
-            HorizontalDivider()
-            TaskOption(stringResource(R.string.repeat), R.drawable.repeat) {}
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                horizontalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                val days = listOf("Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс")
-                days.forEach { day ->
-                    val isSelected = repeatDays.contains(day)
-                    Box(
-                        modifier = Modifier
-                            .size(32.dp)
-                            .clip(CircleShape)
-                            .background(if (isSelected) AccentColor else Color.Unspecified)
-                            .border(2.dp, AccentColor, CircleShape)
-                            .clickable {
-                                repeatDays = if (isSelected) repeatDays - day else repeatDays + day
-                            }, contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = day, style = SimpleText,
-                            color = if (isSelected) Color.Black else InverseColor
-                        )
-                    }
-                }
-            }
-            HorizontalDivider()
-
-            TaskOption(stringResource(R.string.time_for_task), R.drawable.vremya) {
-                TimePickerDialog(
-                    context,
-                    { _, hour, minute ->
-                        taskDuration =
-                            String.format(Locale.getDefault(), "%02d:%02d:00", hour, minute)
-                    }, 0, 45, true
-                ).show()
-            }
-
-            Row(
-                modifier = Modifier
-                    .background(AccentColor, shape = RoundedCornerShape(15.dp))
-                    .padding(8.dp),
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = taskDuration, style = H2, color = InverseColor)
-            }
-            HorizontalDivider()
+//            TaskOption(stringResource(R.string.shopping), R.drawable.proekt, showIcon = true) {}
+//            HorizontalDivider()
+//            TaskOption(stringResource(R.string.reminders), R.drawable.bell) {}
+//
+//            Row(
+//                Modifier
+//                    .fillMaxWidth()
+//                    .padding(8.dp)
+//            ) {
+//                reminders.forEach { reminder ->
+//                    Row(
+//                        modifier = Modifier
+//                            .background(AccentColor, shape = RoundedCornerShape(20.dp))
+//                            .padding(horizontal = 12.dp, vertical = 6.dp),
+//                        verticalAlignment = Alignment.CenterVertically
+//                    ) {
+//                        Text(text = reminder, style = SimpleText, color = DarkerPine)
+//                        Spacer(modifier = Modifier.width(4.dp))
+//                        Icon(
+//                            painter = painterResource(id = R.drawable.krest),
+//                            contentDescription = null, tint = DarkerPine,
+//                            modifier = Modifier
+//                                .size(16.dp)
+//                                .clickable { reminders = reminders - reminder }
+//                        )
+//                    }
+//                    Spacer(modifier = Modifier.width(8.dp))
+//                }
+//            }
+//
+//            HorizontalDivider()
+//            TaskOption(stringResource(R.string.repeat), R.drawable.repeat) {}
+//            Row(
+//                Modifier
+//                    .fillMaxWidth()
+//                    .padding(8.dp),
+//                horizontalArrangement = Arrangement.spacedBy(4.dp)
+//            ) {
+//                val days = listOf("Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс")
+//                days.forEach { day ->
+//                    val isSelected = repeatDays.contains(day)
+//                    Box(
+//                        modifier = Modifier
+//                            .size(32.dp)
+//                            .clip(CircleShape)
+//                            .background(if (isSelected) AccentColor else Color.Unspecified)
+//                            .border(2.dp, AccentColor, CircleShape)
+//                            .clickable {
+//                                repeatDays = if (isSelected) repeatDays - day else repeatDays + day
+//                            }, contentAlignment = Alignment.Center
+//                    ) {
+//                        Text(
+//                            text = day, style = SimpleText,
+//                            color = if (isSelected) Color.Black else InverseColor
+//                        )
+//                    }
+//                }
+//            }
+//            HorizontalDivider()
+//
+//            TaskOption(stringResource(R.string.time_for_task), R.drawable.vremya) {
+//                TimePickerDialog(
+//                    context,
+//                    { _, hour, minute ->
+//                        taskDuration =
+//                            String.format(Locale.getDefault(), "%02d:%02d:00", hour, minute)
+//                    }, 0, 45, true
+//                ).show()
+//            }
+//
+//            Row(
+//                modifier = Modifier
+//                    .background(AccentColor, shape = RoundedCornerShape(15.dp))
+//                    .padding(8.dp),
+//                horizontalArrangement = Arrangement.Start,
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                Text(text = taskDuration, style = H2, color = InverseColor)
+//            }
+//            HorizontalDivider()
         }
     }
 }
